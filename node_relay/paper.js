4da7df410004a7b31696d375b0a15cf68eccb798
Elaine Ou <elaine.ou@gmail.com>
2017-06-02 20:39:08 -0700

    basic paper generator

---
+++ node_relay/paper.js
@@ -0,0 +1,87 @@
+var PDFDocument = require('pdfkit');
+
+module.exports = function(req, res){
+  console.log(req.body)
+
+  //reqs: combine protocol and coin (symbol?), 
+  // optional: name, email
+  /*
+  var title = req.body.protocol;  
+  var email = req.body.email;
+  var protocols = req.body.protocols
+    */
+
+  var token = "ScamMonster";
+  var symbol = "SCAM";
+  var author = "Satoshi";
+  var protocols = ["ethereum"];
+  var title = "A blah blah blah"
+  var email = "elaine@gmail.com"
+
+  var whitepaper = getWhitepaper(protocols);
+  
+
+  var doc = new PDFDocument({ title: token, author: author });
+  
+
+  // Title page
+  doc.font('Times-Bold', 24).text(token + ": ");
+  doc.font('Times-Bold', 18).text(title + ": ");
+  doc.moveDown();
+
+  doc.font('Times-Roman', 15).text(author + "\n" + email);
+  doc.moveDown();
+
+  doc.fillColor(243).font('Times-Roman', 12);
+
+  var sections = Object.keys(whitepaper);
+
+  for (s in sections) {
+    var k = sections[s];
+    var content = whitepaper[k];
+
+    if(content) {
+        doc.moveDown();
+        var header = keywordReplace(k, token, symbol);
+
+        doc.font('Times-Bold', 14).text(header);
+        doc.font('Times-Roman', 12)
+
+
+        for(c in content) {
+            var para = content[c];
+            if (para.img) {
+                doc.moveDown();
+                doc.image(__dirname + para.img).moveDown();
+            } else if (para.code) {
+                doc.moveDown();
+                doc.font('Courier', 12).text(keywordReplace(para.code, token, symbol));
+                doc.moveDown();
+            } else {
+                doc.moveDown();
+                doc.font('Times-Roman', 12).text(keywordReplace(para, token, symbol));
+            }
+        }
+    }
+  }
+
+  doc.end();
+  res.contentType("application/pdf");
+  doc.pipe(res); 
+
+};
+
+function keywordReplace(str, token, symbol) {
+    return str.replace("_PROTOCOL_", token).replace("_COIN_", symbol);
+}
+
+function getWhitepaper(protocols) {
+    var whitepaper = require('./papers/template.js');
+    var ethereum = require('./papers/ethereum.js');
+
+    if (protocols.includes("ethereum")) 
+        Object.assign(whitepaper, ethereum);
+
+    return whitepaper;
+
+}
